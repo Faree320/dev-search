9b26864b1c08b18e6dcfa873ff1b2d9f4580495e
Faree320 <100018647+Faree320@users.noreply.github.com>
2022-08-06 11:22:45 +0500

    Initial commit 1

--- crud_store/settings.py
+++ crud_store/settings.py
@@ -161,6 +161,8 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 django_heroku.settings(locals())
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
